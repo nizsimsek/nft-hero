4a3afab50ee4429d16e8e3e3150f95bd7f386eb7
nizsimsek <nizsimsek@icloud.com>
2022-09-05 03:22:33 +0300

    Deploy Fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+// Deployed on Vercel: https://vercel.com/
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
